59859de33b1058d9e324a578b6a114ed05727325
Kisalay Pan <22f2001094@ds.study.iitm.ac.in>
2025-02-16 15:23:42 +0530

    Update index.ts

--- Frontend_Proto/my-vue-spa/src/router/index.ts
+++ Frontend_Proto/my-vue-spa/src/router/index.ts
@@ -23,6 +23,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/PythonPage.vue') // Dynamically import the LoginPage component
   },
   {
+    path: '/english',
+    name: 'EnglishPage', // Updated name to match the route naming convention
+    component: () => import('../views/EnglishPage.vue') // Dynamically import the LoginPage component
+  },
+  {
     path: '/coding',
     name: 'CodingPage', // Updated name to match the route naming convention
     component: () => import('../views/CodingPage.vue') // Dynamically import the LoginPage component
@@ -38,9 +43,19 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/StudentDashboard.vue') // Dynamically import the StudentDashboard component
   },
   {
+    path: '/instructor-dashboard',
+    name: 'InstructorDashboard', // Updated name to match the route naming convention
+    component: () => import('../views/InstructorDashboard.vue') // Dynamically import the InstructorDashboard component
+  },
+  {
     path: '/python-grades',
     name: 'PythonGrades', // Updated name to match the route naming convention
     component: () => import('../views/PythonGrades.vue') // Dynamically import the LoginPage component
+  },
+  {
+    path: '/english-grades',
+    name: 'EnglishGrades', // Updated name to match the route naming convention
+    component: () => import('../views/EnglishGrades.vue') // Dynamically import the LoginPage component
   }
 ]
 
